18d09e3d22eeef52d0f9d45aa46cb9ce510c83b5
Shyu Lee <shyu.lee@gmail.com>
2020-05-22 09:15:55 -0700

    Added template instructions into new Strategy type

--- Pacmio/Source/Modules/Strategy/Strategy.cs
+++ Pacmio/Source/Modules/Strategy/Strategy.cs
@@ -23,16 +23,38 @@ namespace Pacmio
 
         public string Name { get; }
 
+        // New Types for Bar
+        // ===================
+        // 1. TrendLine Type (Will be derived)
+        // 2. Signal Type
+        // 3. Pattern Type
+        // 4. Position Status
+        // 5. Better Tags
+        // 
+        // <<<<<<<<<< Get them from the Technical Anlysis (TA)'s purpose >>>>>>>>>>
+        // 1. For example, RSI: see how the overbought and oversold are causing price reversals
+        // 2. Moving average crossings. Test difference time periods
+
         // Condition
         // --------------------
         // Result: List of symbol matching the condition
+        // 1. Volume, 
+        // 2. Price Range
+        // 3. Indicators + Elevation Factors
+
+        public Dictionary<Contract, int> Parameters = new Dictionary<Contract, int>();
 
         // Indication
         // --------------------
-        // Elevation Factors
-        // Deprication Factors
+        // Elevation Factors (Bullish)
+        // Deprication Factors (Bearish)
         // Signals
         // Result: Signals list, sum of all signals at the close of a Bar
+        // --------------------
+        // 1. A pool of bullish and bearish factor-tets: <<<<<<<<<< Get them from the Technical Anlysis (TA)'s purpose >>>>>>>>>>
+        // 2. Test each of them individually for each contract. Signal clarity -> bullish signal to bullish upside rate / Bearish to downside rate. Delay, strength, winrate
+        // 3. Classic combination: Moving Average Cross (s) + Oscillator (s)
+        // 4. Combined Score to simulated trades: KPI: winrate, per trade profit
 
         // Confirmation
         // --------------------
@@ -48,5 +70,12 @@ namespace Pacmio
         // 2. Breakeven Stop is met
         // 3. Profit taking limit is met
         // Result: Remove liquidation, scale out, Sell / Cover
+    }
+
+    public class StrategyParameter 
+    {
+        // BarFreq
+        // List of indicators, and indicator parameters
+            
     }
 }
