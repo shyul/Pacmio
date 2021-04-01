9eaedfa82300629607922e4e006f57841ad0c66e
shyul <me@xuli.us>
2021-03-31 22:57:42 -0400

    Update IndicatorEvaluationResult.cs

--- Pacmio/Source/Modules/BarTable/Analysis/06_Indicator/IndicatorEvaluationResult.cs
+++ Pacmio/Source/Modules/BarTable/Analysis/06_Indicator/IndicatorEvaluationResult.cs
@@ -11,71 +11,102 @@ using System.Threading;
 using System.Threading.Tasks;
 using Xu;
 
-namespace Pacmio.Analysis
+namespace Pacmio
 {
     public class IndicatorEvaluationResult
     {
-        public IndicatorEvaluationResult(BarTable bt, IndicatorSet inds) 
+        public IndicatorEvaluationResult(Contract c , IndicatorSet inds)
         {
-            int totalBarCount = bt.Count;
-
-            if (totalBarCount > 0)
-            {
-                var bas = inds[bt];
-                Indicator ind = inds[bt.BarFreq, bt.Type];
-
-                bt.CalculateRefresh(bas);
-
-                BullishBars = bt.Bars.Where(n => n.GetSignalScore(ind).Bullish > ind.BullishPointLimit);
-                BearishBars = bt.Bars.Where(n => n.GetSignalScore(ind).Bearish > ind.BearishPointLimit);
-
-                BullishPercent = BullishBars.Count() * 100 / totalBarCount;
-                BearishPercent = BearishBars.Count() * 100 / totalBarCount;
-
-                BullishBars.RunEach(n => BullishPeriods.Add(n.Period));
-                BearishBars.RunEach(n => BearishPeriods.Add(n.Period));
-            }
+            Contract = c;
+            IndicatorSet = inds;
         }
 
-        public IEnumerable<Bar> BullishBars { get; }
+        public Contract Contract { get; }
 
-        public IEnumerable<Bar> BearishBars { get; }
+        public IndicatorSet IndicatorSet { get; }
 
         public MultiPeriod BullishPeriods { get; } = new MultiPeriod();
 
         public MultiPeriod BearishPeriods { get; } = new MultiPeriod();
 
-        public double BullishPercent { get; } = 0;
+        public double BullishPercent { get; set; } = 0;
 
-        public double BearishPercent { get; } = 0;
+        public double BearishPercent { get; set; } = 0;
     }
 
 
-    public static class IndicatorManager 
+    public static class IndicatorManager
     {
-        public static void Evaluate(IEnumerable<Contract> cList,  CancellationTokenSource cts, IProgress<float> Progress) 
+        /// <summary>
+        /// This function is for narrowing down the group of contracts for actually simulation.
+        /// </summary>
+        /// <param name="cList"></param>
+        /// <param name="inds"></param>
+        /// <param name="evaluateTimeRange"></param>
+        /// <param name="cts"></param>
+        /// <param name="Progress"></param>
+        /// <returns></returns>
+        public static Dictionary<Contract, IndicatorEvaluationResult> Evaluate(IEnumerable<Contract> cList, IndicatorSet inds, Period evaluateTimeRange, CancellationTokenSource cts, IProgress<float> Progress)
         {
+            Dictionary<Contract, IndicatorEvaluationResult> result = new();
+
             double totalseconds = 0;
             float total_num = cList.Count();
             float i = 0;
 
             //BarTableSet bts =
-            Parallel.ForEach(cList, new ParallelOptions { MaxDegreeOfParallelism = 8 },
-                c => {
-                    DateTime startTime = DateTime.Now;
+            Parallel.ForEach(cList, new ParallelOptions { MaxDegreeOfParallelism = 8 }, c =>
+            {
 
-                    BarTableSet bts = new BarTableSet(c, false);
-                    bts.SetPeriod(pd, Cts);
-                    BarTable bt = bts[freq, type];
+                IndicatorEvaluationResult ier = new(c, inds);
 
-                    bt.CalculateRefresh(bas);
-                    DateTime endTime = DateTime.Now;
-                    double seconds = (endTime - startTime).TotalSeconds;
-                    totalseconds += seconds;
-                    i++;
-                    Progress.Report(i * 100.0f / total_num);
-                });
+                DateTime startTime = DateTime.Now;
+                BarTableSet bts = new BarTableSet(c, false);
+                bts.SetPeriod(evaluateTimeRange, cts);
 
+                //Dictionary<>
+
+                // So far we are only getting results from one time frame!
+
+                foreach (var item in inds.Where(n => n.freq >= BarFreq.Daily).OrderByDescending(n => n.freq))
+                {
+                    BarTable bt = bts[item.freq, item.type];
+
+                    Indicator ind = inds[item.freq, item.type];
+
+                    int totalBarCount = bt.Count;
+                    bt.CalculateRefresh(item.bas);
+
+                    var BullishBars = bt.Bars.Where(n => n.GetSignalScore(ind).Bullish > ind.BullishPointLimit);
+                    var BearishBars = bt.Bars.Where(n => n.GetSignalScore(ind).Bearish > ind.BearishPointLimit);
+
+                    ier.BullishPercent = BullishBars.Count() * 100 / totalBarCount;
+                    ier.BearishPercent = BearishBars.Count() * 100 / totalBarCount;
+
+                    BullishBars.RunEach(n => ier.BullishPeriods.Add(n.Period));
+                    BearishBars.RunEach(n => ier.BearishPeriods.Add(n.Period));
+
+                }
+
+                DateTime endTime = DateTime.Now;
+                double seconds = (endTime - startTime).TotalSeconds;
+                totalseconds += seconds;
+                i++;
+                Progress.Report(i * 100.0f / total_num);
+            });
+
+
+            return result;
+        }
+
+        public static void PrintResult(Dictionary<Contract, IndicatorEvaluationResult> result) 
+        {
+            var r = result.OrderByDescending(n => n.Value.BullishPercent).Select(n=>n.Value).Take(100);
+
+            foreach(var ier in r) 
+            {
+                Console.WriteLine(ier.Contract + ": " + ier.BullishPercent);
+            }
         }
     }
 }
