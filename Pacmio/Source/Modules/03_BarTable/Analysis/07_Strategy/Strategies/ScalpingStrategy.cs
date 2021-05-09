d2efc467b6b70c69a93f7ba5d8c5f44954408e8a
shyul <me@xuli.us>
2021-05-08 23:04:24 -0400

    Create ScalpingStrategy.cs

---
+++ Pacmio/Source/Modules/03_BarTable/Analysis/07_Strategy/Strategies/ScalpingStrategy.cs
@@ -0,0 +1,18 @@
+﻿/// ***************************************************************************
+/// Shared Libraries and Utilities
+/// Copyright 2001-2008, 2014-2021 Xu Li - me@xuli.us
+/// 
+/// ***************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xu;
+using Xu.Chart;
+
+namespace Pacmio
+{
+    public class ScalpingStrategy
+    {
+    }
+}
